97f31e5825f75f97b2b832632972e8589515b809
Francisco Zorat <franciscozorat@gmail.com>
2022-11-09 14:25:51 -0300

    play y dependencias

---
+++ src/commands/play.js
@@ -0,0 +1,62 @@
+const Command = require("../structures/command.js");
+
+module.exports = new Command({
+    name: "play",
+    description: "Reproduce la canción o playlist especificada.",
+    options: [
+        { description: 'nombre de la canción/URL', name: 'busqueda', required: true, type: 3 }
+    ],
+    async run(Bot, message, args, slash) {
+
+        const embedReply = (description) => {
+            const embed = { embeds: [{ description }], ephemeral: true, failIfNotExists: false }
+            return slash ? message.editReply(embed) : message.reply(embed);
+        }
+
+        if (!message.member.voice.channelId)
+            return embedReply('No estás en un VC.');
+        if (message.guild.members.me.voice.channelId && message.member.voice.channelId !== message.guild.members.me.voice.channelId)
+            return embedReply('No estás en el mismo VC que yo.');
+
+        if (!args[0]) return;
+
+        /* Si el bot no tiene permisos para entrar al vc */
+        if (!message.guild.members.me.permissionsIn(message.member.voice.channel).has(Bot.requiredVoicePermissions))
+            return embedReply('El bot no tiene permisos para entrar al canal de voz.');
+
+        if (slash) await message.deferReply();
+        let query = args.join(" ");
+        const searchResult = await Bot.player.search(query, { requestedBy: slash ? message.user : message.author, searchEngine: "custom" })
+        if (!searchResult || !searchResult.tracks.length)
+            return embedReply('No encontré nada.');
+
+        const queue = await Bot.player.createQueue(message.guild, {
+            metadata: { channel: message.channel },
+            bufferingTimeout: 1000,
+            disableVolume: false, // disabling volume controls can improve performance
+            leaveOnEnd: true,
+            leaveOnStop: true,
+            spotifyBridge: false
+        });
+        let justConnected;
+        try {
+            if (!queue.connection) {
+                justConnected = true;
+                await queue.connect(message.member.voice.channel);
+            }
+        } catch {
+            Bot.player.deleteQueue(message.guild);
+            return embedReply('No pude joinear tu canal de voz.');
+        }
+
+        searchResult.playlist
+            ? queue.addTracks(searchResult.tracks)
+            : queue.addTrack(searchResult.tracks[0]);
+
+        searchResult.playlist
+            ? embedReply(`Puse en cola **${searchResult.tracks.length}** canciones de [${searchResult.tracks[0].playlist.title}](${searchResult.tracks[0].playlist.url})`)
+            : embedReply(`Puse en cola **[${searchResult.tracks[0].title}](${searchResult.tracks[0].url})**`);
+
+        justConnected && queue.play();
+    }
+});
---
+++ src/events/player/trackEnd.js
@@ -0,0 +1,6 @@
+/* Cuando termina la canción borro el mensaje */
+module.exports = async (player, queue, track) => {
+    if (queue.npmessage && queue.npmessage.editable) {
+        queue.npmessage.delete().catch(console.error);
+    }
+};
