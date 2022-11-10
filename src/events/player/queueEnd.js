47534ea3679e45bb47b1801a55727a24b0bb9350
Francisco Zorat <franciscozorat@gmail.com>
2022-11-10 09:10:55 -0300

    trackEnd/queueEnd

--- src/events/player/queueEnd.js
+++ src/events/player/queueEnd.js
@@ -1,6 +1,6 @@
 /* Cuando termina la queue borro el mensaje */
 module.exports = async (player, queue, track) => {
     if (queue.npmessage && queue.npmessage.editable) {
-        queue.npmessage.delete().catch(console.error);
+        queue.npmessage.delete().catch(() => { });
     }
 };
