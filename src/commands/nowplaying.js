f2a388916175a4aa94385b0685f71c5caac98b6b
Francisco Zorat <franciscozorat@gmail.com>
2023-06-05 16:40:57 -0300

    new commands WIP

---
+++ src/commands/nowplaying.js
@@ -0,0 +1,56 @@
+const Command = require("../structures/command.js");
+const { colors } = require('../constants');
+const { getTrackTitle } = require('../utils/player');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = new Command({
+    name: "nowplaying",
+    aliases: ['np'],
+    description: "Muestra información de lo que se está reproduciendo en este momento.",
+    async run(Bot, message, args, extra = {}) {
+        const queue = Bot.player.nodes.get(message.guild);
+        if (!queue || !queue.playing) {
+            const embed = new EmbedBuilder()
+                .setDescription(`No estoy reproduciendo nada en este server.`);
+            return message.reply({ embeds: [embed] });
+        }
+        const progress = queue.node.createProgressBar({ timecodes: true, length: 8 });
+
+        const track = queue.currentTrack;
+        const title = getTrackTitle(track);
+
+        return message.reply({
+            embeds: [
+                {
+                    author: {
+                        name: title,
+                        url: track.url
+                    },
+                    // description: `**[${title}](${track.url})**\nPedido por ${track.requestedBy}`,
+                    thumbnail: {
+                        url: `${track.thumbnail}`
+                    },
+                    fields: [
+                        {
+                            name: `Pedida por:`,
+                            value: `${track.requestedBy.username}#${track.requestedBy.discriminator}`,
+                            inline: true,
+                        },
+                        {
+                            name: `Views:`,
+                            value: `${track.views}`,
+                            inline: true,
+                        },
+                        {
+                            name: '\u200b',
+                            value: progress.replace(/ 0:00/g, ' ◉ LIVE')
+                        },
+                    ],
+                    footer: { text: `Fraan's Music | Reproduciendo ahora`, iconURL: 'https://cdn-icons-png.flaticon.com/512/183/183625.png' },
+                    color: colors['light-blue']
+                }
+            ]
+        });
+    }
+});
+
