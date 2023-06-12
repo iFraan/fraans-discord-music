215eb8074ee9377183c94670a9daffa2209422e5
Francisco Zorat <franciscozorat@gmail.com>
2023-06-12 12:30:21 -0300

    shuffle command

--- src/commands/shuffle.js
+++ src/commands/shuffle.js
@@ -1,53 +1,33 @@
 const Command = require("../structures/command.js");
 const { colors } = require('../constants');
-const { getTrackTitle } = require('../utils/player');
-const { ActionRowBuilder, EmbedBuilder, StringSelectMenuBuilder } = require('discord.js');
+const { useQueue } = require("discord-player");
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = new Command({
     name: "shuffle",
     aliases: ['sf'],
     description: "Mezcla la queue.",
     async run(Bot, message, args, extra = {}) {
-        const queue = Bot.player.nodes.get(message.guild);
-        if (!queue || !queue.playing) {
-            const embed = new EmbedBuilder()
-                .setDescription(`No estoy reproduciendo nada en este server.`);
+        const queue = useQueue(message.guild);
+        if (!queue || !queue.node.isPlaying()) {
+            const embed = new EmbedBuilder().setDescription(`No estoy reproduciendo nada en este server.`);
             return message.reply({ embeds: [embed] });
         }
 
-        const tracks = queue.tracks.slice(0, 10).map(track => {
-            const title = getTrackTitle(m);
-            return ({
-                label: title,
-                value: track.url,
-                description: track.author,
-            })
-        })
+        if (queue.tracks.data.length === 0) {
+            const embed = new EmbedBuilder().setDescription(`No hay canciones en cola.`);
+            return reply({ embeds: [embed] });
+        }
 
-        const row = new ActionRowBuilder().addComponents(
-            new StringSelectMenuBuilder()
-                .setCustomId("shuffle_select")
-                .setPlaceholder('1')
-                .setMaxValues(1)
-                .addOptions(tracks)
-        );
+        queue.tracks.shuffle();
 
         return message.reply({
             embeds: [
                 {
-                    fields: [
-
-                        {
-                            name: 'name',
-                            value: `value.`
-                        },
-                    ],
-                    footer: { text: `Fraan's Music | Shuffle`, iconURL: 'https://cdn-icons-png.flaticon.com/512/183/183625.png' },
+                    footer: { text: `Shuffle | Mezclé las canciones de la cola.`, iconURL: 'https://cdn-icons-png.flaticon.com/512/183/183625.png' },
                     color: colors['light-blue']
                 }
             ],
-            ephemeral: true,
-            components: [row]
         });
     }
 });
