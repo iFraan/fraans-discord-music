3b03fa748f2b3a5e2e0ae8a2ed3303c1a42baae9
Francisco Zorat <franciscozorat@gmail.com>
2023-06-12 10:53:19 -0300

    dev guild for isnta command updates

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,12 @@ const {
     TOKEN_DISCORD,
     TOKEN_GENIUS,
     DISCORD_CLIENT_ID,
+    DISCORD_DEV_GUILD_ID,
 } = require('./keys.json');
 
 module.exports = {
     TOKEN_DISCORD: TOKEN_DISCORD || '',
     DISCORD_CLIENT_ID: DISCORD_CLIENT_ID || '',
+    DISCORD_DEV_GUILD_ID: DISCORD_DEV_GUILD_ID || '',
     TOKEN_GENIUS: TOKEN_GENIUS || '',
 };
--- src/utils/discordapi.js
+++ src/utils/discordapi.js
@@ -1,4 +1,4 @@
-const { DISCORD_CLIENT_ID, TOKEN_DISCORD } = require('../keys');
+const { DISCORD_CLIENT_ID, DISCORD_DEV_GUILD_ID, TOKEN_DISCORD } = require('../keys');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 
@@ -11,6 +11,8 @@ const publishSlashCommands = async (commands) => {
     }));
     const rest = new REST({ version: '10' }).setToken(TOKEN_DISCORD);
     try {
+        await rest.put(Routes.applicationGuildCommands(DISCORD_CLIENT_ID, DISCORD_DEV_GUILD_ID), { body: slashCommands });
+        console.log(`[INFO] Comandos publicados en discord/dev.`)
         await rest.put(Routes.applicationCommands(DISCORD_CLIENT_ID), { body: slashCommands });
         console.log(`[INFO] Comandos publicados en discord.`)
     } catch (e) {
