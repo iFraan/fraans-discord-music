94cae5af469f713fd4b8d10e0da5f38ee290cf51
Francisco Zorat <franciscozorat@gmail.com>
2023-07-22 04:29:23 -0300

    replace json keys with .env

--- src/keys.js
+++ src/keys.js
@@ -1,13 +1,8 @@
-const {
-    TOKEN_DISCORD,
-    TOKEN_GENIUS,
-    DISCORD_CLIENT_ID,
-    DISCORD_DEV_GUILD_ID,
-} = require('./keys.json');
+require('dotenv').config();
 
 module.exports = {
-    TOKEN_DISCORD: TOKEN_DISCORD || '',
-    DISCORD_CLIENT_ID: DISCORD_CLIENT_ID || '',
-    DISCORD_DEV_GUILD_ID: DISCORD_DEV_GUILD_ID || '',
-    TOKEN_GENIUS: TOKEN_GENIUS || '',
+    TOKEN_DISCORD: process.env.TOKEN_DISCORD ?? '',
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID ?? '',
+    DISCORD_DEV_GUILD_ID: process.env.DISCORD_DEV_GUILD_ID ?? '',
+    TOKEN_GENIUS: process.env.TOKEN_GENIUS ?? '',
 };
