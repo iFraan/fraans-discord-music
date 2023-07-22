035255f792a2ba1dc04299fe2608580cbc2ee933
Francisco Zorat <franciscozorat@gmail.com>
2023-07-22 04:24:55 -0300

    useMasterPlayer is deprecated

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,35 +1,29 @@
 const Command = require("../structures/command.js");
-const { useMasterPlayer } = require('discord-player');
+const { useMainPlayer } = require('discord-player');
 
 module.exports = new Command({
-    name: "play",
-    description: "Reproduce la canción o playlist especificada.",
-    options: [
-        { description: 'nombre de la canción/URL', name: 'busqueda', required: true, type: 3 }
-    ],
+    name: 'play',
+    description: 'Reproduce la canción o playlist especificada.',
+    options: [{ description: 'nombre de la canción/URL', name: 'busqueda', required: true, type: 3 }],
     async run(Bot, message, args, extra = {}) {
-
         const { slash } = extra;
         const embedReply = (description) => {
-            const embed = { embeds: [{ description }], ephemeral: true, failIfNotExists: false }
+            const embed = { embeds: [{ description }], ephemeral: true, failIfNotExists: false };
             return slash ? message.editReply(embed) : message.reply(embed);
-        }
+        };
 
         if (slash) await message.deferReply();
-        if (!message.member.voice.channelId)
-            return embedReply('No estás en un VC.');
-        if (message.guild.members.me.voice.channelId && message.member.voice.channelId !== message.guild.members.me.voice.channelId)
-            return embedReply('No estás en el mismo VC que yo.');
+        if (!message.member.voice.channelId) return embedReply('No estás en un VC.');
+        if (message.guild.members.me.voice.channelId && message.member.voice.channelId !== message.guild.members.me.voice.channelId) return embedReply('No estás en el mismo VC que yo.');
 
         if (!args[0]) return;
 
         /* Si el bot no tiene permisos para entrar al vc */
-        if (!message.guild.members.me.permissionsIn(message.member.voice.channel).has(Bot.requiredVoicePermissions))
-            return embedReply('El bot no tiene permisos para entrar al canal de voz.');
+        if (!message.guild.members.me.permissionsIn(message.member.voice.channel).has(Bot.requiredVoicePermissions)) return embedReply('El bot no tiene permisos para entrar al canal de voz.');
 
-        const query = args.join(" ");
+        const query = args.join(' ');
         const channel = message.member.voice.channel;
-        const player = useMasterPlayer();
+        const player = useMainPlayer();
 
         const searchResult = await player.search(query, { requestedBy: slash ? message.user : message.author });
 
@@ -44,7 +38,7 @@ module.exports = new Command({
                     metadata: {
                         message,
                         requestedBy: slash ? message.user : message.author,
-                        channel: message.channel
+                        channel: message.channel,
                     }, // we can access this metadata object using queue.metadata later on,
                     selfDeaf: true,
                     volume: 80,
@@ -52,20 +46,18 @@ module.exports = new Command({
                     leaveOnEmptyCooldown: 300000,
                     leaveOnEnd: true,
                     leaveOnEndCooldown: 300000,
-                }
-            })
+                },
+            });
 
             const playlist = searchResult.playlist;
 
             playlist
                 ? embedReply(`Puse en cola **${playlist.tracks.length}** canciones de [${playlist.title}](${playlist.url}) (${playlist.source})`)
                 : embedReply(`Puse en cola **[${track.title}](${track.url})** (${track.raw.source})`);
-
         } catch (e) {
             // let's return error if something failed
-            console.log(e)
+            console.log(e);
             return message.followUp(`Something went wrong: ${e}`);
         }
-
-    }
+    },
 });
