605217ff7ee98aba62f69e3f0b57e753b8090171
Francisco Zorat <franciscozorat@gmail.com>
2023-07-22 04:37:48 -0300

    fix shuffle when no songs in queue

--- src/commands/shuffle.js
+++ src/commands/shuffle.js
@@ -8,10 +8,14 @@ module.exports = new Command({
     aliases: ['sf'],
     description: "Mezcla la queue.",
     async run(Bot, message, args, extra = {}) {
+        const { isFromButton = false } = extra;
+        const reply = (content) => {
+            isFromButton ? message.channel.send(content) : message.reply(content);
+        };
         const queue = useQueue(message.guild);
         if (!queue || !queue.node.isPlaying()) {
             const embed = new EmbedBuilder().setDescription(`No estoy reproduciendo nada en este server.`);
-            return message.reply({ embeds: [embed] });
+            return reply({ embeds: [embed] });
         }
 
         if (queue.tracks.data.length === 0) {
@@ -21,12 +25,12 @@ module.exports = new Command({
 
         queue.tracks.shuffle();
 
-        return message.reply({
+        return reply({
             embeds: [
                 {
                     footer: { text: `Shuffle | Mezclé las canciones de la cola.`, iconURL: 'https://cdn-icons-png.flaticon.com/512/183/183625.png' },
-                    color: colors['light-blue']
-                }
+                    color: colors['light-blue'],
+                },
             ],
         });
     }
