ee5ee9c792b72e6d39f52526973399c573bddaad
Francisco Zorat <franciscozorat@gmail.com>
2023-10-31 09:38:41 -0300

    initial lang strings

---
+++ src/languages/es_ar.js
@@ -0,0 +1,10 @@
+module.exports = {
+    player: {
+        notInVc: "No estás en un VC",
+        notSameVc: "No estás en el mismo VC que yo.",
+        notEnoughPermissions: "El bot no tiene permisos para entrar al canal de voz.",
+        noTracks: "No encontré nada. \nProbablemente tenga restricciones de edad o esté bloqueada en este pais.",
+    },
+    addedToQueuePlaylist: "Puse en cola **{SONGS_QUANTITY}** canciones de [{PLAYLIST_TITLE}]({PLAYLIST_URL}) ({PLAYLIST_SOURCE})",
+    addedToQueueSong: "Puse en cola **[{TRACK_TITLE}]({TRACK_URL})** ({TRACK_SOURCE})",
+}
