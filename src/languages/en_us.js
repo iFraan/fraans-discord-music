1f1a6628546da26c9ac3c8799fa7e23da118cb75
Francisco Zorat <franciscozorat@gmail.com>
2023-11-02 10:32:09 -0300

    translate: minor

--- src/languages/en_us.js
+++ src/languages/en_us.js
@@ -2,11 +2,11 @@ module.exports = {
     player: {
         notInVc: "You aren't in a VC",
         notSameVc: "We aren't in the same VC",
-        notEnoughPermissions: "I do not have enough permissions to join this VC.",
-        noTracks: "I didn't find anything. \n Probably this song has age restrictions or is blocked in this country.",
+        notEnoughPermissions: 'I do not have enough permissions to join this VC.',
+        noTracks: "I didn't find anything. \n Probably this song it's age restricted or it's blocked in this country.",
     },
     commands: {
-        notEnoughPermissions: "You dont have enough permissions to use this command. (\`{PERMISSION}\`)",
+        notEnoughPermissions: 'You dont have enough permissions to use this command. (`{PERMISSION}`)',
     },
     addedToQueuePlaylist: "I queue'd up **{SONGS_QUANTITY}** songs from [{PLAYLIST_TITLE}]({PLAYLIST_URL}) ({PLAYLIST_SOURCE})",
     addedToQueueSong: "I queue'd up **[{TRACK_TITLE}]({TRACK_URL})** ({TRACK_SOURCE})",
@@ -14,8 +14,8 @@ module.exports = {
     shuffleQueue: 'I shuffled the queue.',
     notPlaying: "I'm not playing anything on this server.",
     requestedBy: 'Requested by',
-    skipped: 'Skipped **[{TRACK_TITLE}](${TRACK_URL})**',
-    skippedFor: 'Skipped **[{CURRENT_TRACK_TITLE}](${CURRENT_TRACK_URL})** for **[{TRACK_TITLE}]({TRACK_URL})**',
+    skipped: 'Skipped **[{TRACK_TITLE}]({TRACK_URL})**',
+    skippedFor: 'Skipped **[{CURRENT_TRACK_TITLE}]({CURRENT_TRACK_URL})** for **[{TRACK_TITLE}]({TRACK_URL})**',
     skippedBy: 'Skipped by',
     skipList: 'Showing first {SONGS_QUANTITY} songs only',
     disconnectedFromVc: 'Disconnected from the VC',
@@ -23,11 +23,11 @@ module.exports = {
     playingNow: 'Playing now',
     lyrics: {
         noArgs: 'You have to specify the song to search for.',
-        notFound: "Didn't find anything. Are you sure you're writing it right?"
+        notFound: "Didn't find anything. Are you sure you're writing it right?",
     },
     placeholders: {
         chooseLang: 'Choose a language.',
-        chooseFilter: 'Enable or disable audio filters.'
+        chooseFilter: 'Enable or disable audio filters.',
     },
     actions: {
         pause: 'Pause',
@@ -42,6 +42,6 @@ module.exports = {
         views: 'Vies',
         page: 'Page',
         now: 'now',
-        of: 'of'
-    }
-}
+        of: 'of',
+    },
+};
