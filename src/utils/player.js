429c1a1b7378858bd0acd67a37dd2f94ff1b3450
Francisco Zorat <franciscozorat@gmail.com>
2024-01-22 09:21:02 -0300

    undefined?

--- src/utils/player.js
+++ src/utils/player.js
@@ -1,7 +1,7 @@
 const getTrackTitle = (track) => {
     return (['spotify-custom', 'soundcloud-custom'].includes(track?.source)
-        ? `${track.author} - ${track.title}`
-        : `${track.title}`)
+        ? `${track?.author} - ${track?.title}`
+        : `${track?.title}`)
 }
 
 module.exports = {
